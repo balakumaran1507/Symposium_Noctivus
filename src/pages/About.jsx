0859fd50b86c802fa85b3073fe788953493c27eb
balakumaran1507 <145541052+balakumaran1507@users.noreply.github.com>
2025-07-30 15:22:06 +0530

    ss

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,7 +2,10 @@ import { FiCalendar, FiMapPin, FiMail } from "react-icons/fi";
 
 const About = ({ refProp }) => {
   return (
-    <section ref={refProp} className="px-10 lg:px-32 py-10">
+    <section
+      ref={refProp}
+      className="px-10 lg:px-32 py-10 relative overflow-hidden bg-transparent"
+    >
       <div className="mx-auto flex flex-col items-center">
         <div className="font-funnel text-blue-50 w-full flex flex-col items-center overflow-hidden">
           <p className="pb-10 text-sm">NOCTIVUS UNVEILED</p>
@@ -21,7 +24,7 @@ const About = ({ refProp }) => {
 
           {/* Event Details */}
           <div className="flex flex-col md:flex-row items-center md:justify-between w-full gap-6 mt-14 text-white/60 text-sm px-14 text-center md:text-left">
-            <div className="flex items-center gap-2  w-full md:w-auto justify-left">
+            <div className="flex items-center gap-2 w-full md:w-auto justify-left">
               <FiCalendar className="text-emerald-400" />
               <span>August 02, 2025</span>
             </div>
@@ -33,6 +36,16 @@ const About = ({ refProp }) => {
               <FiMail className="text-emerald-400" />
               <span>noctivus.root@gmail.com</span>
             </div>
+          </div>
+
+          {/* Sponsored by Section */}
+          <div className="mt-20 flex flex-col items-centre gap-5 text-xs md:text-sm z-10">
+            <p className="mb-2 text-center">SPONSORED BY</p>
+            <img
+              src="/CH_Logo.jpg"
+              alt="Sponsor Logo"
+              className="h-24 md:h-20 object-contain mix-blend-screen opacity-90"
+            />
           </div>
         </div>
       </div>
